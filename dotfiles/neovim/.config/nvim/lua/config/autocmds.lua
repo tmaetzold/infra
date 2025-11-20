1f746ec8971acaffe395a140bd79c6c0662a6eba
Trent Maetzold <trent@maetzold.co>
2025-11-20 16:01:50 -0600

    fix: delete autocommands for sqlComplete

--- dotfiles/neovim/.config/nvim/lua/config/autocmds.lua
+++ dotfiles/neovim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("Filetype", {
+  pattern = "sql",
+  callback = function()
+    pcall(vim.keymap.del, "i", "<left>", { buffer = true })
+    pcall(vim.keymap.del, "i", "<right>", { buffer = true })
+  end,
+})
