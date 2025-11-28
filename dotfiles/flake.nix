be9bcbda6913fb6e41bcb241d2c525bac1b0df41
Trent Maetzold <trent@maetzold.co>
2025-11-28 17:22:30 -0600

    fix: use 25.05 for nix

--- dotfiles/flake.nix
+++ dotfiles/flake.nix
@@ -2,9 +2,9 @@
   description = "Home Manager configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixpkgs/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixpkgs/nixpkgs/nixos-25.05";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
