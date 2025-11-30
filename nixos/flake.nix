5a0cf7f09dede1bf7594cb399c547d0885aaa9cd
Trent Maetzold <trent@maetzold.co>
2025-11-29 20:25:06 -0600

    refactor: get rid of home-manager config

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,19 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      home-manager,
-    }:
+    { self, nixpkgs }:
     {
       nixosConfigurations = {
         trkm-laptop = nixpkgs.lib.nixosSystem {
@@ -27,13 +18,6 @@
             ./hardware/trkm-laptop.nix
             ./environments/desktop/cinnamon.nix
             ./users/tm.nix
-
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.tm = ../dotfiles/home.nix;
-            }
           ];
         };
       };
--- nixos/users/tm.nix
+++ nixos/users/tm.nix
@@ -1,8 +1,6 @@
 { pkgs, ... }:
 
 {
-  programs.zsh.enable = true;
-
   # Define a user account. Don't forget to set a password with 'passwd'.
   users.users.tm = {
     isNormalUser = true;
@@ -10,8 +8,24 @@
     extraGroups = [
       "networkmanager"
       "wheel"
-      "docker"
+      "podman"
     ];
     shell = pkgs.zsh;
+    packages = with pkgs; [
+      oh-my-zsh
+      pure-prompt
+      zsh
+    ];
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    distrobox
+  ];
+
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;
   };
 }
