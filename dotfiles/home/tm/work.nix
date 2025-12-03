29cea536b91c89d14ef8b60e3b6c0697179c56fb
Trent Maetzold <trent@maetzold.co>
2025-12-03 07:30:35 -0600

    fix: remove jiratui (incompat with nix 25.05)

--- dotfiles/home/tm/work.nix
+++ dotfiles/home/tm/work.nix
@@ -1,14 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  home = {
-    file = {
-      ".config/jiratui".source =
-        config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/infra/dotfiles/jiratui/.config/jiratui";
-    };
-    packages = with pkgs; [ jiratui ];
-  };
-
   programs.git = {
     enable = true;
     userName = "Trent Maetzold";
