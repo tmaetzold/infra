9271545953645a425d772d8e3f3e5efbee5e4fdd
Trent Maetzold <trent@maetzold.co>
2025-12-03 16:15:04 -0600

    fix: use correct variable to exclude files/folders from pyright, change import strategy to bundled so basedpyright uses mason install

--- dotfiles/nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -8,12 +8,13 @@ return {
           enabled = true,
           settings = {
             basedpyright = {
+              importStrategy = "useBundled",
               analysis = {
                 typeCheckingMode = "basic",
                 diagnosticMode = "openFilesOnly",
                 autoSearchPaths = true,
                 useLibraryCodeForTypes = true,
-                ignore = { ".venv", "venv", "**/__pycache__", ".pytest_cache", ".mypy_cache" },
+                exclude = { ".venv", "venv", "**/__pycache__", ".pytest_cache", ".mypy_cache" },
               },
             },
           },
