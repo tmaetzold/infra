feea612726403d8111916b8091a5adb2231ea616
Trent Maetzold <trent@maetzold.co>
2025-12-04 09:14:13 -0600

    refactor: move jiratui config to work profile

--- dotfiles/home/tm/work.nix
+++ dotfiles/home/tm/work.nix
@@ -3,6 +3,15 @@ _:
 {
   imports = [ ./base.nix ];
 
+  xdg.configFile = {
+    "jiratui/config.yaml".text = ''
+      jira_api_username: "tmaetzold_gcp"
+      jira_api_token: ''${JIRA_API_TOKEN}
+      jira_api_base_url: "https://.gardacp.com"
+      jira_api_version: 2
+    '';
+  };
+
   programs.git = {
     enable = true;
     userName = "Trent Maetzold";
