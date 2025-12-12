2003744e63b5fbd22d3219d8185a778832e733e4
Trent Maetzold <trent@maetzold.co>
2025-12-12 11:05:48 -0600

    config: update lspconfig for better basedpyright settings

--- dotfiles/nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -14,13 +14,23 @@ return {
                 diagnosticMode = "openFilesOnly",
                 autoSearchPaths = true,
                 useLibraryCodeForTypes = true,
+                include = {},
                 exclude = { ".venv", "venv", "**/__pycache__", ".pytest_cache", ".mypy_cache" },
-                ---|--------------------------------------------------------------------|
+                ignore = {},
+                -- |--------------------------------------------------------------------|
                 -- | Setting | Files Analyzed? | Errors Shown? | Available for Imports? |
                 -- |---------|-----------------|---------------|------------------------|
                 -- | include | ✅ Yes          | ✅ Yes        | ✅ Yes                 |
                 -- | exclude | ❌ No           | ❌ No         | ⚠️ Only if imported    |
                 -- | ignore  | ✅ Yes          | ❌ No         | ✅ Yes                 |
+                -- |--------------------------------------------------------------------|
+                inlayHints = {
+                  callArgumentNames = false,
+                  callArgumentNamesMatching = false,
+                  functionReturnTypes = false,
+                  genericTypes = false,
+                  variableTypes = false,
+                },
               },
             },
           },
