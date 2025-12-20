900282926b85cb6fc18ffa1b1c88544a8ff1e649
Trent Maetzold <trent@maetzold.co>
2025-12-20 11:59:09 -0600

    nix: revert to 25.05

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,9 +2,9 @@
   description = "NixOS Configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
