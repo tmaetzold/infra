a0bb46d49e304f26f39151407666d677fa2a572c
Trent Maetzold <trent@maetzold.co>
2026-01-07 05:57:17 -0600

    nixos: move nvidia from roles to hardware

---
+++ nixos/hardware/nvidia.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  hardware.graphics.enable = true;
+
+  services.xserver.videoDrivers = lib.mkDefault [ "nvidia" ];
+
+  hardware = {
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement = {
+        enable = lib.mkDefault false;
+        finegrained = lib.mkDefault false;
+      };
+      open = lib.mkDefault true;
+      nvidiaSettings = true;
+      package = lib.mkDefault config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    nvidia-system-monitor-qt
+    nvtopPackages.nvidia
+  ];
+}
