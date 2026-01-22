b5fd5176d005a0ae2d692262e687d084ee7e3d78
Trent Maetzold <trent.maetzold@gardacp.com>
2026-01-22 08:34:40 -0600

    hm: add jiratui, update git format for unstable branch

--- dotfiles/home/tm/work.nix
+++ dotfiles/home/tm/work.nix
@@ -1,4 +1,4 @@
-_:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -10,6 +10,10 @@ _:
     username = "tmaetzold_gcp";
     homeDirectory = "/home/tmaetzold_gcp";
     stateVersion = "25.05";
+    packages = with pkgs; [ jiratui ];
+    shellAliases = {
+      jt = "jiratui ui";
+    };
     sessionVariables = {
       NIX_SSL_CERT_FILE = "/etc/ssl/certs/ca-certificates.crt";
       SSL_CERT_FILE = "/etc/ssl/certs/ca-certificates.crt";
@@ -22,17 +26,20 @@ _:
     "jiratui/config.yaml".text = ''
       jira_api_username: "tmaetzold_gcp"
       jira_api_token: ''${JIRA_API_TOKEN}
-      jira_api_base_url: "https://.gardacp.com"
+      jira_api_base_url: "https://jira.gardacp.com"
       jira_api_version: 2
+      cloud: False
     '';
   };
 
-  programs.git = {
-    enable = true;
-    settings = {
-      user = {
-        name = "Trent Maetzold";
-        email = "trent.maetzold@gardacp.com";
+  programs = {
+    git = {
+      enable = true;
+      settings = {
+        user = {
+          name = "Trent Maetzold";
+          email = "trent.maetzold@gardacp.com";
+        };
       };
     };
   };
